394d289986d3cb15f370ba3b5d54bc8e5103edc0
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2021-02-21 06:52:04 +0100

    Update settings.h
    Add comment

--- src/settings.h
+++ src/settings.h
@@ -1,3 +1,6 @@
+// Copyright (c) F4HWN Armel. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 // Wifi
 #define WIFI_SSID       "Your SSID here"
 #define WIFI_PASSWORD   "Your PASSWORD here"
