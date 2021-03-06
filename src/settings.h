b332b2a27ce7d65f95afb9108a7084f1f85b00cd
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2021-03-06 02:47:30 +0100

    Update settings.h
    Many fix

--- src/settings.h
+++ src/settings.h
@@ -2,17 +2,11 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 // Wifi
-#define WIFI_SSID       "\\_(o)_/"
-#define WIFI_PASSWORD   "petitchaton"
+#define WIFI_SSID       "Your SSID here"
+#define WIFI_PASSWORD   "Your PASSWORD here"
 
 // Call
-#define INDICATIF       "F4HWN"
+#define INDICATIF       "Your CALL"
 
 // Spotnik client
-#define SPOTNIK         "http://192.168.1.99:3000/"
-
-// Call
-//#define INDICATIF       "F1ZPX"
-
-// Spotnik client
-//#define SPOTNIK         "http://gremille.globalis-dev.com:3000/"
+#define SPOTNIK         "http://192.168.1.1:3000/"
