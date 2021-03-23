c5518691e77cbf9cb3d0a89f4875bfe6b67a521f
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2021-03-24 00:06:18 +0100

    Update settings

--- src/settings.h
+++ src/settings.h
@@ -2,11 +2,11 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 // Wifi
-#define WIFI_SSID       "Your SSID here"
-#define WIFI_PASSWORD   "Your PASSWORD here"
+const char *wifi[] = {
+    "Your Wifi SSID", "You Wifi PASSWORD", 
+};
 
-// Call
-#define INDICATIF       "Your CALL"
-
-// Spotnik client
-#define SPOTNIK         "http://192.168.1.1:3000/"
+// Spotnik
+const char *spotnik[] = {
+    "Your CALL", "http://192.168.1.1:3000/", 
+};
