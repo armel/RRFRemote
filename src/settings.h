f8ba1b7373969bb8bcd627d607b3900123effad9
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2021-03-24 04:27:00 +0100

    Improve multiconfig

--- src/settings.h
+++ src/settings.h
@@ -1,12 +1,17 @@
 // Copyright (c) F4HWN Armel. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-// Wifi
-const char *wifi[] = {
-    "Your Wifi SSID", "You Wifi PASSWORD", 
+const char *config[] = {
+    "Your SSID", "Your PASSWORD", "Your SPOTNIK_CALL", "http://192.168.1.1:3000/" 
 };
 
-// Spotnik
-const char *spotnik[] = {
-    "Your CALL", "http://192.168.1.1:3000/", 
+
+/*
+Note that it's possible to configure many configurations, like this
+
+const char *config[] = {
+    "SSID_1", "PASSWORD_1", "SPOTNIK_CALL_1", "http://IP_1:3000/",
+    "SSID_2", "PASSWORD_2", "SPOTNIK_CALL_2", "http://IP_2:3000/", 
+    "SSID_3", "PASSWORD_3", "SPOTNIK_CALL_3", "http://IP_3:3000/" 
 };
+*/
