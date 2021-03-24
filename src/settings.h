7b1d426e703593e489460020cf67926ef4141940
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2021-03-24 04:29:26 +0100

    Improve multiconfig

--- src/settings.h
+++ src/settings.h
@@ -2,7 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 const char *config[] = {
-    "Your SSID", "Your PASSWORD", "Your SPOTNIK_CALL", "http://192.168.1.1:3000/" 
+    "WIFI_SSID", "WIFI_PASSWORD", "SPOTNIK_CALL", "http://192.168.1.1:3000/" 
 };
 
 
@@ -10,8 +10,8 @@ const char *config[] = {
 Note that it's possible to configure many configurations, like this
 
 const char *config[] = {
-    "SSID_1", "PASSWORD_1", "SPOTNIK_CALL_1", "http://IP_1:3000/",
-    "SSID_2", "PASSWORD_2", "SPOTNIK_CALL_2", "http://IP_2:3000/", 
-    "SSID_3", "PASSWORD_3", "SPOTNIK_CALL_3", "http://IP_3:3000/" 
+    "WIFI_SSID_1", "WIFI_PASSWORD_1", "SPOTNIK_CALL_1", "http://IP_1:3000/",
+    "WIFI_SSID_2", "WIFI_PASSWORD_2", "SPOTNIK_CALL_2", "http://IP_2:3000/", 
+    "WIFI_SSID_3", "WIFI_PASSWORD_3", "SPOTNIK_CALL_3", "http://IP_3:3000/" 
 };
 */
