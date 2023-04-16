ff633f2d66135d7caa7d516e87d8f992ab0ed92e
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2023-04-16 04:01:37 +0200

    Change endpoint for propag and move to IDF 6.1.0

--- src/RRFRemote.h
+++ src/RRFRemote.h
@@ -1,7 +1,7 @@
 // Copyright (c) F4HWN Armel. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#define VERSION "3.2.2"
+#define VERSION "3.3.0"
 #define AUTHOR "F4HWN"
 #define NAME "RRFRemote"
 
